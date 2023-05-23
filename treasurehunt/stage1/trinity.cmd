d0710d34b2955b2c43c10d083979406dd5bc416c
Brian Haas <brianjohnhaas@gmail.com>
2023-05-23 13:16:54 -0400

    add

---
+++ treasurehunt/stage1/trinity.cmd
@@ -0,0 +1,1 @@
+ ~/GITHUB/trinityrnaseq/Trinity --left reads.simPE_R76_F300_FR.left.fa --right reads.simPE_R76_F300_FR.right.fa --seqType fa --max_memory 1G --CPU 1 --output trinity_outdir --no_normalize_reads
